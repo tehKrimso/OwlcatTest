59e1279dd7324ed9a0162d6eacceb3c101577cbe
Tim Shuvalov <tashuvalov@gmail.com>
2023-05-14 18:52:31 +0300

    заголовочны файл

--- ivfs.h
+++ ivfs.h
@@ -1,10 +1,28 @@
+#ifndef IVFS_H
+#define IVFS_H
+
 #include <cstdint>
+#include <fstream>
+
 
 namespace TestTask 
 {
     struct File
     {
+        public:
+            const char* name;
+            File* root;
+            bool isRoot;
+            bool isDirectory;
+            bool isWriteOnly;
+            bool isReadOnly;
+            uint64_t start;
+            uint64_t end;
+            uint64_t size;
+            std::fstream fileStream;
+           
     };
+
     struct IVFS 
     {
     public:
@@ -15,3 +33,5 @@ namespace TestTask
         virtual void Close(File* f) = 0;
     };
 }
+
+#endif // IVFS_H
