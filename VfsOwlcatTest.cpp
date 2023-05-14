90c8587162e68a84074f61fd8da36c72a68c3636
Tim Shuvalov <tashuvalov@gmail.com>
2023-05-14 18:57:10 +0300

    main

--- VfsOwlcatTest.cpp
+++ VfsOwlcatTest.cpp
@@ -2,19 +2,40 @@
 //
 
 #include <iostream>
+#include "VFS.cpp"
+#include <vector>
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    TestTask::VFS myVfs;
+
+    //create file
+    TestTask::File* test = myVfs.Create("testRoot/testDir/testDir2/testFile");
+
+    //open file in WriteOnly mode
+    TestTask::File* openedTestWrite = myVfs.Create("testRoot/testDir/testDir2/testFile");
+
+    //create write buffer
+    std::vector<char> writeBuffer(8 * 1000 * 1000, 'a');
+    
+    //write to the file
+    myVfs.Write(openedTestWrite, &writeBuffer[0], writeBuffer.size());
+    
+    //close file
+    myVfs.Close(openedTestWrite);
+
+    //open file for ReadOnlyMode
+    TestTask::File* openedTestRead = myVfs.Open("testRoot/testDir/testDir2/testFile");
+
+    //create read buffer
+    char* readBuff = new char[writeBuffer.size() / 2];
+
+    //read from file
+    myVfs.Read(openedTestRead, &readBuff[0], writeBuffer.size()/2);
+
+    //close file
+    myVfs.Close(openedTestRead);
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
